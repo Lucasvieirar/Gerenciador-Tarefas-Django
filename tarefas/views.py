29fb5cd7d5be9a382db257c2e27a395a84718146
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-05-05 21:12:09 +0000

    cria template e rotas

--- tarefas/views.py
+++ tarefas/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def tarefas_home(request):
-    return HttpResponse("Aqui estão suas tarefas")
+    contexto = {
+        "nome": "Lucas"
+    }
+    return  render(request,'tarefas/home.html', contexto)
 
 def tarefas_adicionar(request):
     return HttpResponse("adcione sua tarefas")
